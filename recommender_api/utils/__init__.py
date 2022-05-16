0fe537405b9fdf49290be98a85346640e7b31b16
taki0412 <taki041210@naver.com>
2022-05-16 15:16:47 +0900

    [ADD] #1 CRUD

--- recommender_api/utils/__init__.py
+++ recommender_api/utils/__init__.py
@@ -36,3 +36,11 @@ class CRUD(Databases):
             self.db.commit()
         except Exception as e:
             print("delete DB err", e)
+
+
+if __name__ == "__main__":
+    db = CRUD()
+    db.insertDB(schema='myschema', table='table', colum='ID', data='유동적변경')
+    print(db.readDB(schema='myschema', table='table', colum='ID'))
+    db.updateDB(schema='myschema', table='table', colum='ID', value='와우', condition='유동적변경')
+    db.deleteDB(schema='myschema', table='table', condition="id != 'd'")
