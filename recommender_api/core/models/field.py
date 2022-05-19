95413e0795dfce0cccab5e92b3ff531c6d84afd2
taki0412 <taki041210@naver.com>
2022-05-19 23:00:49 +0900

    [ADD] #1 field table default 수정

--- recommender_api/core/models/field.py
+++ recommender_api/core/models/field.py
@@ -7,9 +7,9 @@ class Field(Base):
     __tablename__ = "field"
 
     id = Column(BigInteger, primary_key=True)
-    frontend = Column(BIGINT, nullable=True, default=0)
-    backend = Column(BIGINT, nullable=True, default=0)
-    android = Column(BIGINT, nullable=True, default=0)
-    ios = Column(BIGINT, nullable=True, default=0)
-    ai = Column(BIGINT, nullable=True, default=0)
-    embedded = Column(BIGINT, nullable=True, default=0)
+    frontend = Column(BIGINT, nullable=True, default=1)
+    backend = Column(BIGINT, nullable=True, default=1)
+    android = Column(BIGINT, nullable=True, default=1)
+    ios = Column(BIGINT, nullable=True, default=1)
+    ai = Column(BIGINT, nullable=True, default=1)
+    embedded = Column(BIGINT, nullable=True, default=1)
