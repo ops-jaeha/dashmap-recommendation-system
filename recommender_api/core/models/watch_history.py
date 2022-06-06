1cce29f4ea97d9eebb776b6f8cc9a1bbe2c50051
taki0412 <taki041210@naver.com>
2022-06-06 23:32:05 +0900

    [FIX] SERIAL Key

--- recommender_api/core/models/watch_history.py
+++ recommender_api/core/models/watch_history.py
@@ -1,11 +1,12 @@
 # Import Library
-from sqlalchemy import Column, INTEGER, ForeignKey
+from sqlalchemy import Column, INTEGER, ForeignKey, BIGINT
 from recommender_api.core.models import Base
 
 
 class WatchHistory(Base):
     __tablename__ = "watch_history"
 
-    user_id = Column(INTEGER, ForeignKey("member.id"), nullable=True)
-    lecture_id = Column(INTEGER, ForeignKey("lecture.id"), nullable=True)
+    id = Column(BIGINT, primary_key=True, nullable=True)
+    user_id = Column(BIGINT, ForeignKey("member.id"), nullable=True)
+    lecture_id = Column(BIGINT, ForeignKey("lecture.id"), nullable=True)
     ratings = Column(INTEGER)
