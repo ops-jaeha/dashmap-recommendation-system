55333883e0657e48deb6db0ad28f3341f67656d2
taki0412 <taki041210@naver.com>
2022-06-08 23:56:30 +0900

    [ADD] #1 Recommender API

--- recommender_api/apps/recommend.py
+++ recommender_api/apps/recommend.py
@@ -4,8 +4,8 @@ from typing import Optional
 from datetime import datetime
 
 # Import File
-from utils import *
-from utils.recommend import recommend
+from recommender_api.core.models import session_scope
+from recommender_api.utils.recommend import recommend
 
 
 router = APIRouter()
@@ -13,12 +13,20 @@ router = APIRouter()
 
 @router.get("/recommend/{user_id}", status_code=status.HTTP_200_OK, tags=["recommendation"])
 async def user_recommend(user_id: int):
-    url_1, url_2, url_3, url_4, url_5, url_6 = recommend(user_id=user_id)
-    return {
-        "recommend_1": f"{url_1}",
-        "recommend_2": f"{url_2}",
-        "recommend_3": f"{url_3}",
-        "recommend_4": f"{url_4}",
-        "recommend_5": f"{url_5}",
-        "recommend_6": f"{url_6}"
-    }
+    with session_scope() as session:
+        id_lst, url_lst = recommend(session=session, u_id=user_id)
+
+        return {
+            "recommend_1": [{"id": f"{id_lst[0]}"},
+                            {"url": f"{url_lst[0]}"}],
+            "recommend_2": [{"id": f"{id_lst[1]}"},
+                            {"url": f"{url_lst[1]}"}],
+            "recommend_3": [{"id": f"{id_lst[2]}"},
+                            {"url": f"{url_lst[2]}"}],
+            "recommend_4": [{"id": f"{id_lst[3]}"},
+                            {"url": f"{url_lst[3]}"}],
+            "recommend_5": [{"id": f"{id_lst[4]}"},
+                            {"url": f"{url_lst[4]}"}],
+            "recommend_6": [{"id": f"{id_lst[5]}"},
+                            {"url": f"{url_lst[5]}"}]
+        }
